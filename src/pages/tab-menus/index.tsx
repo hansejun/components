9f8aa3bc58bf24092aac5e8f366275ded60bf978
chacha <86880916+hansejun@users.noreply.github.com>
2024-07-22 23:16:17 +0900

    feat:tab

---
+++ src/pages/tab-menus/index.tsx
@@ -0,0 +1,9 @@
+import TabMenus from '../../components/tab-menus/tab-menus';
+
+export default function TabMenusPage() {
+  return (
+    <main>
+      <TabMenus />
+    </main>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import AccordionPage from './pages/accordion';
 import Layout from './components/layout/layout';
 import HomePage from './pages/home';
+import TabMenusPage from './pages/tab-menus';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="/accordion" element={<AccordionPage />} />
+        <Route path="/tabs" element={<TabMenusPage />} />
       </Route>
     </Routes>
   );
