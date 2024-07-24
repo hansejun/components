0e4c17472c0324a658da9b982b65fd80a474e716
hansejun <86880916+hansejun@users.noreply.github.com>
2024-07-24 21:32:46 +0900

    feat: tooltip

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import AccordionPage from './pages/accordion';
 import Layout from './components/layout/layout';
 import HomePage from './pages/home';
 import TabMenusPage from './pages/tab-menus';
+import TooltipPage from './pages/tooltip';
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
         <Route index element={<HomePage />} />
         <Route path="/accordion" element={<AccordionPage />} />
         <Route path="/tabs" element={<TabMenusPage />} />
+        <Route path='/tooltip' element={<TooltipPage/>} />
       </Route>
     </Routes>
   );
